221ac7866a7cda640638311e169b1cffe323e527
sszymko1 <sszymkowiak@edu.cdv.pl>
2019-03-23 13:59:08 +0100

    pierwszy program

--- progStrukturalne.py
+++ progStrukturalne.py
@@ -1,1 +1,58 @@
 print("CDV")
+print(2)
+
+#potęga
+potega = 2 ** 10
+print(potega)
+
+tekst = "CDV"
+print(tekst * 2)
+
+#pobieranie danych z klawiatury
+#imie = input()
+imie = 'test'
+print("Twoje imie podane z klawiatury: " + imie)
+
+nazwisko = 'Szymkowiak'
+print("Twoje dane: " + imie + " " + nazwisko)
+
+dlugosc = len(nazwisko)
+print(dlugosc)
+
+print(type(imie))
+print(type(nazwisko))
+
+#wiek = input()
+wiek = '22'
+print("Wiek: " + wiek)
+
+wiekAla = 30
+print(type(wiekAla))
+wiekAla = str(wiekAla)
+print(type(wiekAla))
+print("Wiek Ali: " + wiekAla)
+
+pierwszyZnak = nazwisko[0]
+print(pierwszyZnak)
+
+ostatniZnak = nazwisko[len (nazwisko) -1]
+print(ostatniZnak)
+
+#konwersja
+x ="5"
+print(type(x))
+x = int(x)
+print(type(x))
+
+y = 4
+print(type(y))
+y = 4 / 2
+# y = float(y)
+print(type(y))
+print(y)
+
+nazwisko = "Kowalski"
+print(nazwisko[0])
+print(nazwisko[0:3])
+print(nazwisko[-2])
+
