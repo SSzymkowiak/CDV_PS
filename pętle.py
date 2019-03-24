2e82725c378a3c71559c11766c08a49fd589e140
sszymko1 <sszymkowiak@edu.cdv.pl>
2019-03-24 15:59:00 +0100

    pętle

--- pętle.py
+++ pętle.py
@@ -1,1 +1,23 @@
-da
+#while
+licznik = 0
+while (licznik < 3):
+    licznik = licznik + 1
+    print("CDV")
+print(licznik)
+
+############################
+
+licznik = 0
+while (licznik < 3):
+    licznik += 1
+    print(licznik, end="")
+else:
+    print("Koniec pętli")
+
+##############################
+##########   FOR
+
+lista = ["Jan","Anna","Paweł"]
+
+for i in lista:
+    print(i)
