749d0a38fdd70dcfac55db342b919367f086cbb1
sszymko1 <sszymkowiak@edu.cdv.pl>
2019-03-24 15:38:55 +0100

    funkcje

---
+++ listy2.py
@@ -0,0 +1,26 @@
+#listy
+programowanie = ["PHP", "Python", "Java"]
+print(type(programowanie))
+programowanie.append("C#")
+programowanie.append("PHP")
+print(programowanie)
+ile = programowanie.count("PHP")
+print("Ile: ", ile)
+
+#tuple
+
+imiona = ("Julia","Anna","Tomek")
+print(type(imiona))
+print(imiona[0])
+
+
+#słownik
+osoba = {"imie":"Julia","nazwisko":"Nowak","wiek":20}
+print(osoba)
+print(type(osoba))
+
+#print(osoba[0])   #błąd
+print(ososba["imie"])
+print(osoba.keys())
+print(osoba.get("wzrost", NULL))
+print(osobs.get("naziwsko", NULL))
