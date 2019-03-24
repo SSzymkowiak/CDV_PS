866f302b1a577cfa920cd2e55ccbcf4097df3ef1
sszymko1 <sszymkowiak@edu.cdv.pl>
2019-03-24 15:45:45 +0100

    funkcje

---
+++ funkcje.py
@@ -0,0 +1,23 @@
+def witaj():
+    print("Witaj ", end="")
+    print("Janusz!")
+
+witaj()
+
+def wyswietlWiek(wiek):
+    print("Masz", wiek, "lat")
+
+wyswietlWiek(20)
+
+def iloczyn(a, b):
+    return a*b
+
+print(iloczyn(2, 3))
+
+def iloraz(a, b):
+    return a / b
+
+print(iloraz(5, 2))
+print(iloraz(2, 5))
+print(iloraz(b=2, a=5))
+
